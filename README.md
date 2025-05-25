07dbdb0224f185470fbb75a15ce399e61a427805
bddjr <bddjr08@outlook.com>
2025-05-25 11:55:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# makeccx-tiny
+
+仅压缩 `src` 文件夹，无其它功能，相当于没有框架。
+
+> ✅ 开发环境占用小  
+> ✅ 打包速度快  
+> ❌ 不支持 `require("clipcc-extension")`，需用 `self.ClipCCExtension` 代替  
+> ❌ 不支持 多个 JS 文件  
+> ❌ 不支持 语言文件特殊写法  
+> ❌ 不支持 node_modules  
+> ❌ 不支持 TypeScript  
+> ❌ 不支持 ES Module 语法  
+> ❌ 不支持 语法检查  
+> ❌ 不支持 Minify（压缩 JS 文件）  
+> ❌ 不支持 `makeccx.config.ts`  
+> ❌ 不支持 `makeccx.config.js`
+
+## 开始
+
+> 1. 确保已安装 [Python 3](https://www.python.org)
+> 2. 下载 zip: https://github.com/makeccx/makeccx-tiny/archive/refs/heads/main.zip
+> 3. 创建文件夹，例如 `clipcc-extension-example`
+> 4. 解压到文件夹里
+> 5. 使用 [VSCode](https://code.visualstudio.com) 打开文件夹
+> 6. 修改 `src/info.json` 和 `src/main.js`
+> 7. 新建终端，运行命令 `./makeccx`
---
+++ src/main.js
@@ -0,0 +1,40 @@
+const { api, type, Extension } = self.ClipCCExtension
+
+const extension_id = "unknown.extension"
+
+const categoryId = extension_id + '.category'
+
+
+const blocks = [
+    {
+        categoryId,
+        opcode: categoryId + '.string',
+        messageId: categoryId + '.string',
+        type: type.BlockType.REPORTER,
+        param: {
+            v: {
+                type: type.ParameterType.STRING,
+                default: 'Hello World!'
+            }
+        },
+        function(args, util) {
+            return String(args.v)
+        },
+    },
+]
+
+
+module.exports = class extends Extension {
+    onUninit() {
+        api.removeCategory(categoryId)
+    }
+    onInit() {
+        api.removeCategory(categoryId)
+        api.addCategory({
+            categoryId,
+            messageId: categoryId,
+            color: '#339900',
+        })
+        api.addBlocks(blocks);
+    }
+}
